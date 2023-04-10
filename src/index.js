e61f942330c0d39a8891985669d8caf727f042f8
mnaibei <muchajulius@gmail.com>
2023-04-10 10:42:05 +0300

    Made results board scrollable, cleared input fields on user submit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import './style.css';
 import { postScores, fetcher } from './modules/post_fetch.js';
 
 const submitForm = document.querySelector('.submit');
+const scoreValue = document.querySelector('.score');
+const nameValue = document.querySelector('.name');
 
 submitForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const scoreValue = document.querySelector('.score').value;
-  const nameValue = document.querySelector('.name').value;
-  postScores(nameValue, scoreValue);
+  postScores(nameValue.value, scoreValue.value);
+  scoreValue.value = ''
+  nameValue.value = ''
 });
 
 const refresh = document.querySelector('.refresh');
